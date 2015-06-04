fe7d0531b621c97794c88dc9f2b540d8cf317e4c
damzst <damzst@gmail.com>
2015-06-03 22:37:11 -0300

    Cleaning folders

--- code/py/trash/warp/source/interp2lin.m
+++
@@ -1,5 +0,0 @@
-function sph = interp2lin(arg1,arg2,arg3,arg4,arg5)
-% M-function wrapper for parsing the inputs:
-
-% Call the MEX-function with the verified inputs:
-sph = interp2linmx(arg1,arg2,arg3,arg4,arg5); 
--- code/py/trash/warp/source/unified_theta.m
+++
@@ -1,5 +0,0 @@
-function Theta = unified_theta(LM,r)
-l = LM(1); m = LM(2);
-Theta = acos(((l+m)*sqrt(r.^2*(1-l^2)+(l+m)^2)-l*r.^2)./...
-          (r.^2 + (l+m)^2) );
-end
